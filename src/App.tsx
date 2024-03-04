913cc6c4210f7906fad5a17887796ead264804c0
Shara <shara.shahverdian@gmail.com>
2024-03-04 23:11:24 +0330

    Enhance UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,12 @@
-import React, { useState } from "react";
 import Display from "./components/Display";
-// import Input from "./components/Input";
 
 function App() {
-  const [number, setNumber] = useState(1);
-  const [name, setName] = useState("F1");
-  const [color, setColor] = useState("#e8002d");
-
-  const onNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const numberValue = parseInt(e.target.value);
-    const clampedValue = Math.max(0, Math.min(99, numberValue));
-    setNumber(clampedValue);
-  };
-
-  const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const nameValue = e.target.value.toUpperCase().slice(0, 3);
-    setName(nameValue);
-  };
-
-  const onColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setColor(e.target.value);
-  };
-
   return (
     <div
-      style={{ height: "100vh" }}
-      className="d-flex flex-column justify-content-center align-items-center gap-5"
+      style={{ height: "100dvh" }}
+      className="d-flex justify-content-center align-items-center"
     >
-      <Display number={number} name={name} color={color} />
-      <div className="d-flex align-items-center">
-        <input
-          className="number-input text-center"
-          type="number"
-          value={number}
-          onChange={onNumberChange}
-        />
-        <input
-          className="name-input text-center"
-          type="text"
-          value={name}
-          onChange={onNameChange}
-        />
-        <input type="color" value={color} onChange={onColorChange} />
-      </div>
+      <Display />
     </div>
   );
 }
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,19 +1,66 @@
+import { useState } from "react";
 type Display = {
-  number: number;
-  name: string;
+  number?: number;
+  name?: string;
   style?: React.CSSProperties;
-  color: string;
+  color?: string;
 };
 
 function Display(props: Display) {
+  const [number, setNumber] = useState(44);
+  const [name, setName] = useState("HAM");
+  const [color, setColor] = useState("#27f4d2");
+  const [isColorVisible, setIsColorVisible] = useState(false);
+
+  const onNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const numberValue = parseInt(e.target.value);
+    const clampedValue = Math.max(0, Math.min(99, numberValue));
+    setNumber(clampedValue);
+  };
+
+  const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nameValue = e.target.value.toUpperCase().slice(0, 3);
+    setName(nameValue);
+  };
+
+  const onColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setColor(e.target.value);
+  };
+
+  const onMouseEnter = () => {
+    setIsColorVisible(isColorVisible ? false : true);
+  };
+
+  const onMouseLeave = () => {
+    setIsColorVisible(isColorVisible ? false : true);
+  };
   return (
-    <div style={{ backgroundColor: props.color }} className="display d-flex">
-      <p className="display-number flex-fill text-center">{props.number}</p>
-      <div>
-        <p style={{ color: props.color }} className="display-name">
-          {props.name}
-        </p>
-      </div>
+    <div
+      style={{ backgroundColor: props.color || color }}
+      className="display d-flex justify-content-between position-relative"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <input
+        className="display-number text-center"
+        type="number"
+        value={number}
+        onChange={onNumberChange}
+      />
+      <input
+        style={{ color: props.color || color }}
+        className="display-name text-center"
+        type="text"
+        value={name}
+        onChange={onNameChange}
+      />
+      <input
+        style={{ visibility: isColorVisible ? "visible" : "hidden" }}
+        className="position-absolute colorpicker"
+        type="color"
+        value={color}
+        onChange={onColorChange}
+      />
     </div>
   );
 }
